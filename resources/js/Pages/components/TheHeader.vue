87d41905c3d79a048772c6780e2973d91224df2c
Mateusz Owczarek <mateuszowczarek@onet.eu>
2024-05-08 09:51:59 +0200

    :bug: removed a from navbar logo and replaced with Link

--- resources/js/Pages/components/TheHeader.vue
+++ resources/js/Pages/components/TheHeader.vue
@@ -1,19 +1,16 @@
 <script setup>
-import { router, Link } from '@inertiajs/vue3'
+import { Link } from '@inertiajs/vue3';
 defineProps({
     user: Object,
 });
 
-function logout() {
-    router.post('/logout')
-}
 </script>
 <template>
     <header class="text-center bg-white/10 mx-4 rounded-xl xl:p-2 mb-4 mt-2 h-auto">
         <nav class="flex items-center flex-wrap xl:flex-row flex-col xl:gap-0">
             <div
                 class="text-2xl xl:border-r-2 border-white/15 pr-2 hover:bg-white/20 p-2 rounded transition-colors duration-300 font-bold">
-                <a href="/"><i class="fa fa-home"></i> Home Page</a>
+                <Link href="/" method="get"><i class="fa fa-home"></i> Home Page</Link>
             </div>
             <div class="xl:mb-0 space-x-4 font-semibold  xl:ml-4">
                 <a href="#"
