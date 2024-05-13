279756e2061eef0a714c01710c7930a129521c57
Mateusz Owczarek <mateuszowczarek@onet.eu>
2024-05-13 18:43:57 +0200

    :bug: fixed bug in BookmarkLink component

--- resources/js/Pages/BookmarkLink.vue
+++ resources/js/Pages/BookmarkLink.vue
@@ -5,7 +5,7 @@ defineProps({
 </script>
 
 <template>
-        <a href="bookmark.url" target="_blank"
+        <a :href="bookmark.url" target="_blank"
            class="font-semibold text-lg hover:text-purple-400 transition-colors duration-300">{{
                 bookmark.name
             }}</a>
